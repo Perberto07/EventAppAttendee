fe3427a44aa73e15c9f7f54c7c3636262819fe1a
johnb <johnb@MSI>
2025-08-06 18:43:44 +0800

    Add project files.

---
+++ EventApp.Shared/notes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventApp.Shared
+{
+    internal class notes
+    {
+        /*
+         Event Booking System
+organizer
+-Book an event or create
+-manage event
+-manage set layout
+-organizer need to login when modifying their even
+
+attendee
+-avail tickets to specific event
+-see available sets
+-pay for event
+
+event
+-timea
+-location
+-available sets
+-description
+-organizer
+-maximum sets
+
+
+anyone can see all available events
+         */
+    }
+}
